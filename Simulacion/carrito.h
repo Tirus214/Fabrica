e47248bfcc398ae65c4641b71e1478c7359a415b
Tirus214 <63261362+Tirus214@users.noreply.github.com>
2020-11-29 23:16:58 -0600

    run carrito2

--- Simulacion/carrito.h
+++ Simulacion/carrito.h
@@ -12,32 +12,53 @@ public:
     QString ingrediente;
     int cantMax;
     int cantMin;
-    int chocolate;
-    int harina;
-    bool peticion;
+    bool chocolate;
+    int peticion;
     double velocidad;
     bool running;
     Almacen * almacen;
+    int resultado;
 
     Carrito(){
-        harina = 0;
-        chocolate = 0;
+        chocolate = true;
         cantMax = 0;
         cantMin = 0;
         velocidad = 0.0;
         almacen = new Almacen();
         running = true;
+        peticion = 0;
+        resultado = 0;
     }
 
     void setAlmacen(Almacen * _almacen);
 
     void traerChocolate(){
-        cho
+        if(cantMax < peticion < cantMin){
+            almacen->chocolate -= peticion;
+        }
+        else peticion = 0;
+    }
+
+    void traerHarina(){
+        if(cantMax < peticion < cantMin){
+            almacen->harina -= peticion;
+        }
+        else peticion = 0;
+    }
+
+    void putResultado(){
+        resultado = peticion;
     }
 
 
     void run(){
-
+        while(running){
+            sleep(velocidad/2*1000);
+            if(chocolate) traerChocolate();
+            else traerHarina();
+            sleep(velocidad/2*1000);
+            putResultado();
+        }
     }
 };
 #endif // CARRITO_H
