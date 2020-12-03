9b99f3e3a72a78f2099584d98d8e9a9f754c098f
adrian <aumana@ic-itcr.ac.cr>
2020-12-03 04:37:32 -0600

    corre

--- build-Simulacion-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-Simulacion-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[174];
+    QByteArrayData data[11];
+    char stringdata0[236];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,7 +39,10 @@ QT_MOC_LITERAL(3, 34, 25), // "on_btnMezcladora1_pressed"
 QT_MOC_LITERAL(4, 60, 25), // "on_btnMezcladora2_pressed"
 QT_MOC_LITERAL(5, 86, 25), // "on_btnMezcladora3_pressed"
 QT_MOC_LITERAL(6, 112, 30), // "on_btnBarraTransporte1_pressed"
-QT_MOC_LITERAL(7, 143, 30) // "on_btnBarraTransporte2_pressed"
+QT_MOC_LITERAL(7, 143, 30), // "on_btnBarraTransporte2_pressed"
+QT_MOC_LITERAL(8, 174, 19), // "on_btnStart_clicked"
+QT_MOC_LITERAL(9, 194, 19), // "on_btnPause_clicked"
+QT_MOC_LITERAL(10, 214, 21) // "on_btnDetener_clicked"
 
     },
     "MainWindow\0on_btnAlmacen_clicked\0\0"
@@ -47,7 +50,9 @@ QT_MOC_LITERAL(7, 143, 30) // "on_btnBarraTransporte2_pressed"
     "on_btnMezcladora2_pressed\0"
     "on_btnMezcladora3_pressed\0"
     "on_btnBarraTransporte1_pressed\0"
-    "on_btnBarraTransporte2_pressed"
+    "on_btnBarraTransporte2_pressed\0"
+    "on_btnStart_clicked\0on_btnPause_clicked\0"
+    "on_btnDetener_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -57,7 +62,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -65,14 +70,20 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x08 /* Private */,
-       3,    0,   45,    2, 0x08 /* Private */,
-       4,    0,   46,    2, 0x08 /* Private */,
-       5,    0,   47,    2, 0x08 /* Private */,
-       6,    0,   48,    2, 0x08 /* Private */,
-       7,    0,   49,    2, 0x08 /* Private */,
+       1,    0,   59,    2, 0x08 /* Private */,
+       3,    0,   60,    2, 0x08 /* Private */,
+       4,    0,   61,    2, 0x08 /* Private */,
+       5,    0,   62,    2, 0x08 /* Private */,
+       6,    0,   63,    2, 0x08 /* Private */,
+       7,    0,   64,    2, 0x08 /* Private */,
+       8,    0,   65,    2, 0x08 /* Private */,
+       9,    0,   66,    2, 0x08 /* Private */,
+      10,    0,   67,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -95,6 +106,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 3: _t->on_btnMezcladora3_pressed(); break;
         case 4: _t->on_btnBarraTransporte1_pressed(); break;
         case 5: _t->on_btnBarraTransporte2_pressed(); break;
+        case 6: _t->on_btnStart_clicked(); break;
+        case 7: _t->on_btnPause_clicked(); break;
+        case 8: _t->on_btnDetener_clicked(); break;
         default: ;
         }
     }
@@ -130,13 +144,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 9;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 9)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 9;
     }
     return _id;
 }
